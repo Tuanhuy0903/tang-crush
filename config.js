851afbb44555fea90eae6e09061e309aade02967
Tuanhuy0903 <86725931+Tuanhuy0903@users.noreply.github.com>
2021-06-30 20:21:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Nào nào trả lời hộ câu này nào:))',
+    desc: 'Mày có chịu làm công chúa của tao cả đời không?:)',
+    btnYes: 'ô kê luôn <3',
+    btnNo: 'Không, Anh trai à :3(bố m thách m chọn đc này)',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'chơi xong trò này đừng cảm động quá nhá hihi',
+    btnAccept: 'nhấn để yêu anh hơn:))',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
